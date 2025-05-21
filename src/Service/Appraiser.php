63cd3697ab4a285018453708be5cd1ca74fa8dcc
Charles Sena <charles.sena.sa@gmail.com>
2025-05-21 17:23:46 -0300

    feat(appraiser): Add `getTheGreatestValues` getter

--- src/Service/Appraiser.php
+++ src/Service/Appraiser.php
@@ -3,11 +3,13 @@
 namespace Charles\Auction\Service;
 
 use Charles\Auction\Model\Auction;
+use Charles\Auction\Model\Bid;
 
 class Appraiser
 {
   private float $greatestValue = -INF;
   private float $lowestValue = INF;
+  private array $greatestValues = [];
 
   public function evaluate(Auction $auction) : void
   {
@@ -15,6 +17,13 @@ class Appraiser
       if($bid->getValue() > $this->greatestValue) $this->greatestValue = $bid->getValue();
       if($bid->getValue() < $this->lowestValue) $this->lowestValue = $bid->getValue();
     }
+
+    $bids = $auction->getBids();
+
+    usort($bids, function(Bid $bid1, Bid $bid2) {
+      return $bid1->getValue() - $bid2->getValue();
+    });
+    $this->greatestValues = array_slice($bids,0,3);
   }
 
   public function getGreatestValue() : float
@@ -26,5 +35,14 @@ class Appraiser
   {
     return $this->lowestValue;
   }
+
+  /**
+   * @return Bid[]
+   */
+  public function getTheGreatestValues() : array
+  {
+    return $this->greatestValues;
+  }
+
 }
 
