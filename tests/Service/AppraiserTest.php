f45e44a20a1c769f89a1b5e04fea52909886291a
Charles Sena <charles.sena.sa@gmail.com>
2025-05-22 14:50:28 -0300

    test(appraiser): Update and Add test methods

--- tests/Service/AppraiserTest.php
+++ tests/Service/AppraiserTest.php
@@ -10,7 +10,7 @@ use Charles\Auction\Model\Bid;
 
 class AppraiserTest extends TestCase
 {
-  public function testOne() : void
+  public function testAppraiserMustGetTheGreatestBidsAtAscendingOrder() : void
   {
     $auction = new Auction('Fiat Uno 0Km');
     $mary = new User('Mary');
@@ -26,6 +26,25 @@ class AppraiserTest extends TestCase
 
     // Assert - Then / Verify if the output was the expected
     $this->assertEquals(2500, $auctioneer->getGreatestValue());
+
+  }
+
+  public function testAppraiserMustGetTheLowestBidsAtAscendingOrder() : void
+  {
+    $auction = new Auction('Fiat Uno 0Km');
+    $mary = new User('Mary');
+    $john = new User('John');
+
+    $auction->placeBid(new Bid($john, 2000));
+    $auction->placeBid(new Bid($mary, 2500));
+
+    $auctioneer = new Appraiser;
+
+    // Act - When / Code to be executed
+    $auctioneer->evaluate($auction);
+
+    // Assert - Then / Verify if the output was the expected
+    $this->assertEquals(2000, $auctioneer->getLowestValue());
 
   }
 }
