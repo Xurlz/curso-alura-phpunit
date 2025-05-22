f2f4f358c8cc9dbdd841251754c9364e0481a2c1
Charles Sena <charles.sena.sa@gmail.com>
2025-05-22 15:36:27 -0300

    test(appraiser): Add `getTheGreatestValues` test method

--- tests/Service/AppraiserTest.php
+++ tests/Service/AppraiserTest.php
@@ -85,5 +85,32 @@ class AppraiserTest extends TestCase
     $this->assertEquals(2500, $auctioneer->getGreatestValue());
 
   }
+
+  public function testAppraiserMustFindThe3GreatestValues() : void
+  {
+    $auction = new Auction('Fiat Uno 0Km');
+
+    $alice = new User('Alice');
+    $bob = new User('Bob');
+    $carl = new User('Carl');
+    $doug = new User('Doug');
+    $edward = new User('Edward');
+
+    $auction->placeBid(new Bid($alice, 1500));
+    $auction->placeBid(new Bid($bob, 1000));
+    $auction->placeBid(new Bid($carl, 2000));
+    $auction->placeBid(new Bid($edward, 1700));
+
+    $auctioneer = new Appraiser;
+    $auctioneer->evaluate($auction);
+
+    $theGreatest = $auctioneer->getTheGreatestValues();
+
+    $this->assertCount(3, $theGreatest);
+    $this->assertEquals(2000, $theGreatest[0]->getValue());
+    $this->assertEquals(1700, $theGreatest[1]->getValue());
+    $this->assertEquals(1500, $theGreatest[2]->getValue());
+
+  }
 }
 
