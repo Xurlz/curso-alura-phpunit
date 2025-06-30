9ee3cd4644294d01eb2cc87a8723cbfc963abfb0
Charles Sena <charles.sena.sa@gmail.com>
2025-06-30 15:06:49 +0000

    refactor(appraiser): Add use of data providers

--- tests/Service/AppraiserTest.php
+++ tests/Service/AppraiserTest.php
@@ -10,15 +10,11 @@ use Charles\Auction\Model\Bid;
 
 class AppraiserTest extends TestCase
 {
-  public function testAppraiserMustGetTheGreatestBidsAtAscendingOrder() : void
+  /**
+   * @dataProvider deliverAuctions
+   */
+  public function testAppraiserMustGetTheGreatestBidsAtAscendingOrder(Auction $auction) : void
   {
-    $auction = new Auction('Fiat Uno 0Km');
-    $mary = new User('Mary');
-    $john = new User('John');
-
-    $auction->placeBid(new Bid($john, 2000));
-    $auction->placeBid(new Bid($mary, 2500));
-
     $auctioneer = new Appraiser;
 
     // Act - When / Code to be executed
@@ -29,14 +25,11 @@ class AppraiserTest extends TestCase
 
   }
 
-  public function testAppraiserMustGetTheLowestBidsAtAscendingOrder() : void
+  /**
+   * @dataProvider deliverAuctions
+   */
+  public function testAppraiserMustGetTheLowestBidsAtAscendingOrder(Auction $auction) : void
   {
-    $auction = new Auction('Fiat Uno 0Km');
-    $mary = new User('Mary');
-    $john = new User('John');
-
-    $auction->placeBid(new Bid($john, 2000));
-    $auction->placeBid(new Bid($mary, 2500));
 
     $auctioneer = new Appraiser;
 
@@ -44,30 +37,15 @@ class AppraiserTest extends TestCase
     $auctioneer->evaluate($auction);
 
     // Assert - Then / Verify if the output was the expected
-    $this->assertEquals(2000, $auctioneer->getLowestValue());
+    $this->assertEquals(1700, $auctioneer->getLowestValue());
 
   }
 
-  public function testAppraiserMustGetTheGreatestBidsAtDescendingOrder() : void
-  {
-    $auction = new Auction('Fiat Uno 0Km');
-    $mary = new User('Mary');
-    $john = new User('John');
 
-    $auction->placeBid(new Bid($mary, 2500));
-    $auction->placeBid(new Bid($john, 2000));
-
-    $auctioneer = new Appraiser;
-
-    // Act - When / Code to be executed
-    $auctioneer->evaluate($auction);
-
-    // Assert - Then / Verify if the output was the expected
-    $this->assertEquals(2500, $auctioneer->getGreatestValue());
-
-  }
-
-  public function testAppraiserMustGetTheLowestBidsAtDescendingOrder() : void
+  /**
+   * @dataProvider deliverAuctions
+   */
+  public function testAppraiserMustGetTheGreatestBidsAtDescendingOrder(Auction $auction) : void
   {
     $auction = new Auction('Fiat Uno 0Km');
     $mary = new User('Mary');
@@ -86,31 +64,107 @@ class AppraiserTest extends TestCase
 
   }
 
-  public function testAppraiserMustFindThe3GreatestValues() : void
+  /**
+   * @dataProvider deliverAuctions
+   */
+  public function testAppraiserMustGetTheLowestBidsAtDescendingOrder(Auction $auction) : void
   {
     $auction = new Auction('Fiat Uno 0Km');
+    $mary = new User('Mary');
+    $john = new User('John');
 
-    $alice = new User('Alice');
-    $bob = new User('Bob');
-    $carl = new User('Carl');
-    $doug = new User('Doug');
-    $edward = new User('Edward');
+    $auction->placeBid(new Bid($mary, 2500));
+    $auction->placeBid(new Bid($john, 2000));
 
-    $auction->placeBid(new Bid($alice, 1500));
-    $auction->placeBid(new Bid($bob, 1000));
-    $auction->placeBid(new Bid($carl, 2000));
-    $auction->placeBid(new Bid($edward, 1700));
+    $auctioneer = new Appraiser;
 
+    // Act - When / Code to be executed
+    $auctioneer->evaluate($auction);
+
+    // Assert - Then / Verify if the output was the expected
+    $this->assertEquals(2500, $auctioneer->getGreatestValue());
+
+  }
+
+  /**
+   * @dataProvider deliverAuctions
+   */
+  public function testAppraiserMustFindThe3GreatestValues(Auction $auction) : void
+  {
     $auctioneer = new Appraiser;
     $auctioneer->evaluate($auction);
 
     $theGreatest = $auctioneer->getTheGreatestValues();
 
     $this->assertCount(3, $theGreatest);
-    $this->assertEquals(2000, $theGreatest[0]->getValue());
-    $this->assertEquals(1700, $theGreatest[1]->getValue());
-    $this->assertEquals(1500, $theGreatest[2]->getValue());
+    $this->assertEquals(2500, $theGreatest[0]->getValue());
+    $this->assertEquals(2000, $theGreatest[1]->getValue());
+    $this->assertEquals(1700, $theGreatest[2]->getValue());
 
+  }
+
+  /**
+   * @return array<array<Auction>>
+   */
+  public function deliverAuctions() : array
+  {
+    return [
+      $this->auctionAtAscendingOrder(),
+      $this->auctionAtDescendingOrder(),
+      $this->auctionAtScrambledOrder()
+    ];
+  }
+
+  /**
+   * @return array<Auction>
+   */
+  public function auctionAtAscendingOrder() : Array
+  {
+    $auction = new Auction('Fiat Uno 200000Km');
+    $alice = new User('Alice');
+    $bob = new User('Bob');
+    $carl = new User('Carl');
+
+    $auction->placeBid(new Bid($alice, 1700));
+    $auction->placeBid(new Bid($bob, 2000));
+    $auction->placeBid(new Bid($carl, 2500));
+
+    return [$auction];
+  }
+
+  /**
+   * @return array<Auction>
+   */
+  public function auctionAtDescendingOrder() : Array
+  {
+    $auction = new Auction('Fiat Uno 200000Km');
+    $alice = new User('Alice');
+    $bob = new User('Bob');
+    $carl = new User('Carl');
+
+    $auction->placeBid(new Bid($carl, 2500));
+    $auction->placeBid(new Bid($bob, 2000));
+    $auction->placeBid(new Bid($alice, 1700));
+
+    return [$auction];
+  }
+
+
+  /**
+   * @return array<Auction>
+   */
+  public function auctionAtScrambledOrder() : Array
+  {
+    $auction = new Auction('Fiat Uno 200000Km');
+    $alice = new User('Alice');
+    $bob = new User('Bob');
+    $carl = new User('Carl');
+
+    $auction->placeBid(new Bid($bob, 2000));
+    $auction->placeBid(new Bid($carl, 2500));
+    $auction->placeBid(new Bid($alice, 1700));
+
+    return [$auction];
   }
 }
 
